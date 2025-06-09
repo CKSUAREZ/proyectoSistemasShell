3ca4b81441fa9d0bf9a0ad93cb3b2ec31fa81e41
CKSUAREZ <angel.sl3110@gmail.com>
2025-06-08 22:20:21 -0600

    correcion

--- shell_eb.c
+++ shell_eb.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <errno.h>
-#include <sys/wait.h>
 
 //*****************************************************************
 // CONSTANTES
